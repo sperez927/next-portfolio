48d9838c4c5d9ed9bb58902b75975d02be528fc7
sperez927 <salverez90@gmail.com>
2025-05-28 22:35:47 -0500

    fix warning

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -2,7 +2,7 @@ import { FC, memo } from 'react';
 
 import { TimelineItem } from '../../../data/dataDef';
 
-const TimelineItem: FC<{ item: TimelineItem }> = memo(({ item }) => {
+const _TimelineItem: FC<{ item: TimelineItem }> = memo(({ item }) => {
   const { title, date, location, content } = item;
   return (
     <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
@@ -19,5 +19,5 @@ const TimelineItem: FC<{ item: TimelineItem }> = memo(({ item }) => {
   );
 });
 
-TimelineItem.displayName = 'TimelineItem';
-export default TimelineItem;
+_TimelineItem.displayName = 'TimelineItem';
+export default _TimelineItem;
