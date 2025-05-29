9b9214f7e4453f31a632cbf0e682defcc91247ab
sperez927 <salverez90@gmail.com>
2025-05-28 22:22:59 -0500

    fix some dependencies

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -8,12 +8,12 @@ import {
   SparklesIcon,
 } from '@heroicons/react/24/outline';
 
-import GithubIcon from '../../../next-portfolio/src/components/Icon/GithubIcon';
+import GithubIcon from '../components/Icon/GithubIcon';
 // import InstagramIcon from '../components/Icon/InstagramIcon';
-import DevCommunityIcon from '../../../next-portfolio/src/components/Icon/DevCommunityIcon';
-import LinkedInIcon from '../../../next-portfolio/src/components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../../../next-portfolio/src/components/Icon/StackOverflowIcon';
-import TwitterIcon from '../../../next-portfolio/src/components/Icon/TwitterIcon';
+import DevCommunityIcon from '../components/Icon/DevCommunityIcon';
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
--- src/data/dataDef.ts
+++ src/data/dataDef.ts
@@ -1,7 +1,7 @@
 import { StaticImageData } from 'next/image';
 import { FC, ForwardRefExoticComponent, SVGProps } from 'react';
 
-import { IconProps } from '../../../next-portfolio/src/components/Icon/Icon';
+import { IconProps } from '../components/Icon/Icon';
 
 export interface HomepageMeta {
   title: string;
